4ae989254b361ec5c8ee791c4eef5a4129536f58
Shmulik <52025854+Masortim@users.noreply.github.com>
2021-12-23 20:39:46 +0700

    Perl Esoteric

---
+++ hello_esoteric.pl
@@ -0,0 +1,6 @@
+qq chop lc and print chr ord uc q chop uc and print chr ord q ne sin and 
+print chr ord qw q le q and print chr ord q else and print chr ord q pop 
+and print chr oct oct ord uc qw q bind q and print chr ord q q eq and print 
+chr ord qw q warn q and print chr ord q pop and print chr ord q qr q and 
+print chr ord q else and print chr ord qw q do q and print chr hex length 
+q q semctl setpgrp chop q
