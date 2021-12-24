a3e7c017c03461b26372e7a27a14343252455636
Shmulik <52025854+Masortim@users.noreply.github.com>
2021-12-24 11:01:14 +0700

    Lisp Esoteric

---
+++ hello_esoteric.lisp
@@ -0,0 +1,8 @@
+(eval (cons (quote mapcar) 
+            (cons (cons (quote function)
+                        (cons (quote princ) ()))
+                  (cons (cons (quote quote)
+                              (cons (cons #\H (cons #\e (cons #\l (cons #\l (cons #\o 
+                                     (cons #\, (cons #\Space 
+                                      (cons #\w (cons #\o (cons #\r (cons #\l (cons #\d (cons #\! 
+                                       ()))))))))))))) ())) ()))))
